8cda94835b8e8ba72e3ce79c06f711f2d3e38cce
IN22915210 <mdsadiq.hussain@tesco.com>
2020-09-22 19:06:11 +0530

    first commit

---
+++ src/main/java/com/example/azuredockerapp/AzureDockerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.azuredockerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureDockerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureDockerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/azuredockerapp/AzureDockerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azuredockerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureDockerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
